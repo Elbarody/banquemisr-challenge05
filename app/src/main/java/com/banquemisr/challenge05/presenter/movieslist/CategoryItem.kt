d347409e80f959e6b650cadc4a667dffebeb00ae
Elbarody <ahmedelbarody999@gmail.com>
2024-06-17 05:36:30 +0300

    change item clickable by animation

--- app/src/main/java/com/banquemisr/challenge05/presenter/movieslist/CategoryItem.kt
+++ app/src/main/java/com/banquemisr/challenge05/presenter/movieslist/CategoryItem.kt
@@ -1,12 +1,17 @@
 package com.banquemisr.challenge05.presenter.movieslist
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -25,9 +30,18 @@ fun CategoryItem(
 ) {
     val isSelected = selectedIndex == index
 
-    val backgroundColor = if (isSelected) Color.Blue else Color.White
-    val textColor = if (isSelected) Color.White else Color.DarkGray
-    val borderColor = if (isSelected) Color.Blue else Color.DarkGray
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isSelected) Color.Blue else Color.White,
+        animationSpec = tween(durationMillis = 500)
+    )
+    val textColor by animateColorAsState(
+        targetValue = if (isSelected) Color.White else Color.DarkGray,
+        animationSpec = tween(durationMillis = 500)
+    )
+    val borderColor by animateColorAsState(
+        targetValue = if (isSelected) Color.Blue else Color.DarkGray,
+        animationSpec = tween(durationMillis = 500)
+    )
 
     Text(
         modifier = Modifier
@@ -41,7 +55,9 @@ fun CategoryItem(
                 color = borderColor
             )
             .padding(horizontal = 10.dp, vertical = 4.dp)
-            .clickable {
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() }, indication = null
+            ) {
                 onItemClicked(index)
             },
         text = category,
