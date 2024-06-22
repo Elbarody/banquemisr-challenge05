5bbd8e9beb2eba7595e7eca5ae000689f4dac26c
Elbarody <ahmedelbarody999@gmail.com>
2024-06-22 09:10:04 +0300

    loader for list

--- app/src/main/java/com/banquemisr/challenge05/presenter/movieslist/MoviesList.kt
+++ app/src/main/java/com/banquemisr/challenge05/presenter/movieslist/MoviesList.kt
@@ -1,10 +1,14 @@
 package com.banquemisr.challenge05.presenter.movieslist
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,14 +18,28 @@ import com.banquemisr.challenge05.data.entity.MovieListItem
 
 @Composable
 fun MoviesList(movies: LazyPagingItems<MovieListItem>, onItemClick: (Int) -> Unit) {
-    LazyRow(
-        modifier = Modifier,
-        horizontalArrangement = Arrangement.spacedBy(6.dp),
-        contentPadding = PaddingValues(4.dp),
-        state = rememberLazyListState()
-    ) {
-        items(movies.itemCount) { index ->
-            MovieItem(movies[index], onItemClick)
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (movies.loadState.refresh is LoadState.Loading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        } else {
+            LazyRow(
+                modifier = Modifier.align(Alignment.Center),
+                horizontalArrangement = Arrangement.spacedBy(6.dp),
+                contentPadding = PaddingValues(4.dp),
+                state = rememberLazyListState()
+            ) {
+                items(movies.itemCount) { index ->
+                    MovieItem(movies[index], onItemClick)
+                }
+
+                if (movies.loadState.append == LoadState.Loading) {
+                    item {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
         }
     }
 }
